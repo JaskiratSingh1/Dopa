c1158ecfe7d63e39a313f8d9fd6a159f8f7f2230
Jaskirat Singh <38223354+JaskiratSingh1@users.noreply.github.com>
2025-05-29 14:48:13 -0600

    Initial Commit

---
+++ Dopa/DopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DopaApp.swift
+//  Dopa
+//
+//  Created by Jaskirat Singh on 2025-05-29.
+//
+
+import SwiftUI
+
+@main
+struct DopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
